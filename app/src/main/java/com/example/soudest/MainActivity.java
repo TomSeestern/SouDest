fef14c189b48d0e21810a5172090af53f4e9335b
TomSeestern <tomsegbers@googlemail.com>
2019-05-23 20:47:50 +0200

    Refactoring Tab Names

--- app/src/main/java/com/example/soudest/MainActivity.java
+++ app/src/main/java/com/example/soudest/MainActivity.java
@@ -16,14 +16,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.planning:
+                    mTextMessage.setText(R.string.title_tab_planning);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.tickets:
+                    mTextMessage.setText(R.string.title_tab_tickets);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.profile:
+                    mTextMessage.setText(R.string.title_tab_profile);
                     return true;
             }
             return false;
