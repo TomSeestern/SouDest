bfb77b486ed8d6d834d9730079152d4bf6318529
TomSeestern <tomsegbers@googlemail.com>
2019-07-19 20:03:05 +0200

    Add Search Button to Pullup Menu

--- app/src/main/java/com/example/soudest/planner_pullup_menu.java
+++ app/src/main/java/com/example/soudest/planner_pullup_menu.java
@@ -43,6 +43,7 @@ public class planner_pullup_menu extends Fragment implements View.OnClickListene
 
     private Button dateText;
     private Button timeText;
+    private Button mysearchbutton;
     private ImageView myswitchIcon;
 
     private int year, month, day;
@@ -124,7 +125,8 @@ public class planner_pullup_menu extends Fragment implements View.OnClickListene
             }
         });
 
-
+        mysearchbutton = (Button) rootView.findViewById(R.id.searchbutton);
+        mysearchbutton.setOnClickListener(this);
 
         //Switch Icon
         myswitchIcon = (ImageView) rootView.findViewById(R.id.switchIcon);
@@ -216,7 +218,16 @@ public class planner_pullup_menu extends Fragment implements View.OnClickListene
                 temp= src.getText().toString();
                 src.setText(dest.getText());
                 dest.setText(temp);
+                break;
 
+            case R.id.searchbutton:
+                if(src.getText().toString().equals("") || dest.getText().toString().equals(""))
+                {
+                    Toast.makeText(getContext().getApplicationContext(), "Bitte Ziel und Startort angeben.", Toast.LENGTH_SHORT).show();
+                }else {
+                    planner_pullup_trippicker trippicker_fragment = (planner_pullup_trippicker) getChildFragmentManager().findFragmentById(R.id.TripPickFragment);
+                    trippicker_fragment.getTrips(src.getText().toString(),dest.getText().toString());
+                }
                 break;
 
             case R.id.dateButton:
